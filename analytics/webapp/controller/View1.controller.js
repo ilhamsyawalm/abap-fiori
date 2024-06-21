a2d641c0964b5d6fa5fab1c9a666d512045f937b
Zhephyr <ilhamsyawal87@gmail.com>
2024-06-21 17:51:34 +0700

    Bikin analytical di SAPUI5 MVC

--- analytics/webapp/controller/View1.controller.js
+++ analytics/webapp/controller/View1.controller.js
@@ -1,40 +1,80 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/suite/ui/commons/ChartContainer",
+    "sap/suite/ui/commons/ChartContainerContent",
+    "sap/ui/vbm/AnalyticMap",
+    "sap/ui/vbm/Spot",
+    "sap/m/MessageToast",
+  ],
+  function (
+    Controller,
+    ChartContainer,
+    ChartContainerContent,
+    AnalyticMap,
+    Spot,
+    MessageToast
+  ) {
+    "use strict";
 
-  return Controller.extend("analytics.controller.View1", {
-    onInit: function () {
-      this.getOwnerComponent()
-        .getRouter()
-        .attachRoutePatternMatched(this.geoMapCollection, this);
-    },
-    geoMapCollection: function () {
-      var oDataModel = this.getView().getModel();
-      var x = this;
+    return Controller.extend("analytics.controller.View1", {
+      onInit: function () {
+        var oView = this.getView();
 
-      oDataModel.read("/geomapSet", {
-        success: function (data) {
-          var aMapData = [];
+        this.adjustMyChartBox(oView, "idVizFrame1", "idCell1");
+        this.adjustMyChartBox(oView, "idVizFrame2", "idCell2");
+        this.adjustMyChartBox(oView, "idVizFrame3", "idCell3");
+        this.adjustMyChartBox(oView, "idVizFrame4", "idCell4");
 
-          for (var i = 0; i < data.results.length; i++) {
-            var item = data.results[i];
+        this.getOwnerComponent()
+          .getRouter()
+          .attachRoutePatternMatched(this.geoMapCollection, this);
+      },
 
-            const r = Math.round(Math.random() * 255);
-            const g = Math.round(Math.random() * 255);
-            const b = Math.round(Math.random() * 255);
+      adjustMyChartBox: function (oView, sChartId, sBlockId) {
+        var oVizFrame = oView.byId(sChartId);
+        var oChartContainerContent = new ChartContainerContent({
+          content: [oVizFrame],
+        });
+        console.log("hai semuanya");
+        var oChartContainer = new ChartContainer({
+          content: [oChartContainerContent],
+        });
 
-            var color = "rgb(" + r + "," + g + "," + b + ")";
-            let color2 = 'rgb(0,255,0)';
-            item.color = color;
-            aMapData.push(item);
-            console.log(color2);
-          }
-          var oModel = new sap.ui.model.json.JSONModel();
-          oModel.setData({
-            mapData: aMapData,
-          });
-          x.getView().setModel(oModel, "viewModel");
-        },
-      });
-    },
-  });
-});
+        oChartContainer.setShowFullScreen(true);
+        oChartContainer.setAutoAdjustHeight(true);
+        oView.byId(sBlockId).addContent(oChartContainer);
+      },
+
+      geoMapCollection: function () {
+        var oDataModel = this.getView().getModel();
+        var x = this;
+
+        oDataModel.read("/geomapSet", {
+          success: function (data) {
+            var aMapData = [];
+
+            for (var i = 0; i < data.results.length; i++) {
+              var item = data.results[i];
+
+              const r = Math.round(Math.random() * 255);
+              const g = Math.round(Math.random() * 255);
+              const b = Math.round(Math.random() * 255);
+
+              var color = "rgb(" + r + "," + g + "," + b + ")";
+              let color2 = "rgb(0,255,0)";
+              item.color = color;
+              aMapData.push(item);
+              console.log(color2);
+            }
+            var oModel = new sap.ui.model.json.JSONModel();
+            oModel.setData({
+              mapData: aMapData,
+            });
+            x.getView().setModel(oModel, "viewModel");
+          },
+        });
+      },
+    });
+  }
+);
