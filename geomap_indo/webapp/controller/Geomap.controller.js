32dfd37f1160ea77d439662d86a0cfe2369f93cf
Kamikaze <ilhamsyawal87@gmail.com>
2024-10-02 17:44:30 +0700

    Selesai semuanya, tinggal pelajari penggunaan dialog

--- geomap_indo/webapp/controller/Geomap.controller.js
+++ geomap_indo/webapp/controller/Geomap.controller.js
@@ -5,8 +5,22 @@ sap.ui.define(
     "sap/ui/model/json/JSONModel",
     "sap/ui/Device",
     "sap/m/MessageToast",
+    "sap/m/Popover",
+    "sap/m/Text",
+    "sap/m/Button",
+    "sap/m/Dialog",
   ],
-  function (AnalyticMap, Controller, JSONModel, Device, MessageToast) {
+  function (
+    AnalyticMap,
+    Controller,
+    JSONModel,
+    Device,
+    MessageToast,
+    Popover,
+    Text,
+    Button,
+    Dialog
+  ) {
     "use strict";
 
     AnalyticMap.GeoJSONURL =
@@ -24,30 +38,34 @@ sap.ui.define(
         let oData = this.getView().getModel();
         let x = this;
 
+        let legend = [
+          { Info: "high", warna: "rgb(56, 163, 56)" },
+          { Info: "medium", warna: "rgb(249,172,60)" },
+          { Info: "low", warna: "rgb(243,0,0)" },
+        ];
+
         oData.read("/Zilham_001_idn", {
           success: function (data) {
             for (let i = 0; i < data.results.length; i++) {
               let isian = [];
               let color = "";
               let type = "";
-              // let Success = rgb(255, 255, 255);
               isian = data.results[i];
               // console.log(data);
-
               // Menambahkan data yg di butuhkan untuk geomap
               let pos = `${data.results[i].longtitude};${data.results[i].latitude};0`;
 
               if (data.results[i].sales >= 6600000000) {
-                color = "rgb(56, 163, 56)";
+                color = legend[0].warna;
                 type = "Success";
               } else if (
                 data.results[i].sales >= 3300000000 &&
                 data.results[i].sales < 6600000000
               ) {
-                color = "rgb(249, 172, 60)";
+                color = legend[1].warna;
                 type = "Warning";
               } else {
-                color = "rgb(243,0,0)";
+                color = legend[2].warna;
                 type = "Error";
               }
 
@@ -55,14 +73,15 @@ sap.ui.define(
               isian.color = color;
               isian.pos = pos;
               Isi.push(isian);
-              // console.log(Isi);
             }
+            console.log(Isi);
             var oModel = new sap.ui.model.json.JSONModel();
 
-            // Set array data ke model
-            oModel.setData({ products: Isi });
+            oModel.setData({
+              products: Isi,
+              legends: legend,
+            });
 
-            // Mengikat model ke view
             x.getView().setModel(oModel, "productModel");
           },
         });
@@ -78,6 +97,41 @@ sap.ui.define(
             );
           }
         }
+      },
+
+      onSpotClick: function (e) {
+        let resultTitle;
+        let resultDetail;
+        let detail = e.getSource();
+        let detailTooltip = detail.getText();
+
+        for (let i = 0; i < Isi.length; i++) {
+          if (Isi[i].tooltip == detailTooltip) {
+            resultTitle = Isi[i].city;
+            resultDetail = `Memiliki id code : ${Isi[i].id_code} \n
+                            dengan titik spot : (${Isi[i].pos}) \n
+                            dan Nilai penjualan sebesar : ${
+                              Isi[i].sales / 100
+                            }`;
+          }
+        }
+        if (!this.popover) {
+          this.popover = new Dialog({
+            title: resultTitle,
+            content: new Text({ text: resultDetail }),
+            beginButton: new Button({
+              text: "Close",
+              press: function () {
+                this.popover.close();
+              }.bind(this),
+            }),
+          });
+        }
+        this.getView().addDependent(this.popover);
+        this.popover.setTitle(resultTitle);
+        this.popover.getContent()[0].setText(resultDetail);
+
+        this.popover.open();
       },
     });
   }
