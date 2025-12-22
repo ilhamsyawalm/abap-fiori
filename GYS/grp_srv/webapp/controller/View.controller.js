4ed1ae42443865b0be09b3a758f54c1bd1cdab06
Kamikaze <ilhamsyawal87@gmail.com>
2025-12-22 15:49:26 +0700

    update logic untuk grp_srv

--- GYS/grp_srv/webapp/controller/View.controller.js
+++ GYS/grp_srv/webapp/controller/View.controller.js
@@ -9,69 +9,103 @@ sap.ui.define(
     return Controller.extend("grpsrv.grpsrv.controller.View", {
       onInit: function () {
         let oModel = this.getOwnerComponent().getModel();
+        let oData = this.getView().getModel("data");
+        let oContext = this;
 
-        //Mengambil data dari OData service dengan filter tertentu
-        oModel.read("/getMtsStocksSet", {
-          filters: [
-            new sap.ui.model.Filter(
-              "Material",
-              sap.ui.model.FilterOperator.EQ,
-              "RBFF"
-            ),
-            new sap.ui.model.Filter(
-              "Sloc",
-              sap.ui.model.FilterOperator.EQ,
-              "*"
-            ),
-            new sap.ui.model.Filter(
-              "Plant",
-              sap.ui.model.FilterOperator.EQ,
-              "CBTG"
-            ),
-          ],
-          success: function (data) {
-            console.log("Data berhasil diambil:", data);
-          },
-          error: function (err) {
-            console.error("Gagal mengambil data:", err);
-          },
-        });
+        //? //Mengambil data dari OData service dengan filter tertentu
+        // oModel.read("/getMtsStocksSet", {
+        //   filters: [
+        //     new sap.ui.model.Filter(
+        //       "Material",
+        //       sap.ui.model.FilterOperator.EQ,
+        //       "RBFF"
+        //     ),
+        //     new sap.ui.model.Filter(
+        //       "Sloc",
+        //       sap.ui.model.FilterOperator.EQ,
+        //       "*"
+        //     ),
+        //     new sap.ui.model.Filter(
+        //       "Plant",
+        //       sap.ui.model.FilterOperator.EQ,
+        //       "CBTG"
+        //     ),
+        //   ],
+        //   success: function (data) {
+        //     console.log("Data berhasil diambil:", data);
+        //   },
+        //   error: function (err) {
+        //     console.error("Gagal mengambil data:", err);
+        //   },
+        // });
 
-        //Mengambil data dari OData service tanpa filter
-        oModel.read("/wbmsWgtDataSet", {
-          success: function (data) {
-            console.log("Data tanpa filter berhasil diambil:", data);
-          },
-          error: function (err) {
-            console.error("Gagal mengambil data tanpa filter:", err);
-          },
-        });
+        //? //Mengambil data dari OData service tanpa filter
+        // oModel.read("/wbmsWgtDataSet", {
+        //   success: function (data) {
+        //     console.log("Data tanpa filter berhasil diambil:", data);
+        //   },
+        //   error: function (err) {
+        //     console.error("Gagal mengambil data tanpa filter:", err);
+        //   },
+        // });
 
-        //Memanggil function import OData service dengan parameter tertentu
-        oModel.callFunction("/getBonOrder", {
-          method: "GET",
-          urlParameters: {
-            BonOrdNo: "231211BON001",
-          },
-          success: function (data) {
-            console.log("Data getBonOrder berhasil diambil:", data);
-          },
-          error: function (err) {
-            console.error("Gagal mengambil data getBonOrder:", err);
-          },
-        });
+        //? //Memanggil function import OData service dengan parameter tertentu
+        // oModel.callFunction("/getBonOrder", {
+        //   method: "GET",
+        //   urlParameters: {
+        //     BonOrdNo: "231211BON001",
+        //   },
+        //   success: function (data) {
+        //     console.log("Data getBonOrder berhasil diambil:", data);
+        //   },
+        //   error: function (err) {
+        //     console.error("Gagal mengambil data getBonOrder:", err);
+        //   },
+        // });
 
+        // oModel.callFunction("/getDoData", {
+        //   method: "GET",
+        //   urlParameters: {
+        //     ChangedDate: "20250917",
+        //     DlvNo: "",
+        //     DlvType: "ZDRG",
+        //     // DlvType: "ZDST",
+        //   },
+        //   success: function (data) {
+        //     console.log("Data getDoData berhasil diambil:", data);
+        //     console.log(data.results);
+        //   },
+        //   error: function (err) {
+        //     console.error("Gagal mengambil data getDoData:", err);
+        //   },
+        // });
+
+        let oToday = new Date();
+        // Change format ke YYYYMMDD
+        let sChangedDate =
+          oToday.getFullYear().toString() +
+          String(oToday.getMonth() + 1).padStart(2, "0") +
+          String(oToday.getDate()).padStart(2, "0");
+
+        this.byId("ChangeDate").setValue(sChangedDate);
+        console.log(oToday);
         oModel.callFunction("/getDoData", {
           method: "GET",
           urlParameters: {
-            ChangedDate: "20250917",
+            ChangedDate: sChangedDate,
             DlvNo: "",
-            DlvType: "ZDRG",
-            // DlvType: "ZDST",
+            DlvType: "",
           },
-          success: function (data) {
-            console.log("Data getDoData berhasil diambil:", data);
-            console.log(data.results);
+          success: function (hasil) {
+            console.log("Data getDoData berhasil diambil:", hasil);
+            let result = hasil.results;
+            oContext.getView().setModel(oData, "result");
+
+            if (result.length === 0) {
+              sap.m.MessageToast.show("Data tidak ditemukan");
+            } else {
+              sap.m.MessageToast.show("Data berhasil diambil");
+            }
           },
           error: function (err) {
             console.error("Gagal mengambil data getDoData:", err);
