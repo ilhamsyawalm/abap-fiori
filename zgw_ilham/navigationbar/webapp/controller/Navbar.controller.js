e1965b46cb54360643b89f967d1afd5aeb042d70
Kamikaze <ilhamsyawal87@gmail.com>
2025-12-27 11:00:13 +0700

    update

--- zgw_ilham/navigationbar/webapp/controller/Navbar.controller.js
+++ zgw_ilham/navigationbar/webapp/controller/Navbar.controller.js
@@ -26,14 +26,89 @@ sap.ui.define(
   ) {
     "use strict";
 
+    var ButtonType = library.ButtonType,
+      PlacementType = library.PlacementType,
+      NavigationListItemDesign = tntLibrary.NavigationListItemDesign;
+
     return Controller.extend("navigationbar.controller.Navbar", {
       onInit: function () {
         var oModel = new sap.ui.model.json.JSONModel(
           "https://ilhamsyawalm.github.io/abap-fiori/zgw_ilham/navigationbar/webapp/model/Data.json"
         );
 
-        console.log(oModel);
         this.getView().setModel(oModel, "jsonModel");
+      },
+
+      adjustMyChartBox: function (oView, sChartId, sBlockId) {
+        var oVizFrame = oView.byId(sChartId);
+        var oChartContainerContent = new ChartContainerContent({
+          content: [oVizFrame],
+        });
+        var oChartContainer = new ChartContainer({
+          content: [oChartContainerContent],
+        });
+
+        oChartContainer.setShowFullScreen(true);
+        oChartContainer.setAutoAdjustHeight(true);
+        oView.byId(sBlockId).addContent(oChartContainer);
+      },
+
+      // Saat side navigation digunakan
+      onItemSelect: function (oEvent) {
+        var oItem = oEvent.getParameter("item");
+        this.byId("pageContainer").to(this.getView().createId(oItem.getKey()));
+      },
+
+      // Memasukan button baru saat nama di click
+      handleUserNamePress: function (event) {
+        var oPopover = new Popover({
+          showHeader: false,
+          placement: PlacementType.Bottom,
+          content: [
+            new Button({
+              text: "Feedback",
+              type: ButtonType.Transparent,
+            }),
+            new Button({
+              text: "Help",
+              type: ButtonType.Transparent,
+            }),
+            new Button({
+              text: "Logout",
+              type: ButtonType.Transparent,
+            }),
+          ],
+        }).addStyleClass("sapMOTAPopover sapTntToolHeaderPopover");
+
+        oPopover.openBy(event.getSource());
+      },
+
+      // Alert saat user di click
+      onAvatarPressed: function () {
+        MessageToast.show("Hello everyone! :)");
+      },
+
+      onFixedPressed: function () {
+        MessageToast.show("Fixed navigation got press! :)");
+      },
+
+      // Untuk hamburger side navigation
+      onSideNavButtonPress: function () {
+        var oToolPage = this.byId("toolPage");
+        var bSideExpanded = oToolPage.getSideExpanded();
+
+        this._setToggleButtonTooltip(bSideExpanded);
+
+        oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+      },
+
+      _setToggleButtonTooltip: function (bLarge) {
+        var oToggleButton = this.byId("sideNavigationToggleButton");
+        if (bLarge) {
+          oToggleButton.setTooltip("Large Size Navigation");
+        } else {
+          oToggleButton.setTooltip("Small Size Navigation");
+        }
       },
     });
   }
